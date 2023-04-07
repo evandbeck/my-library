fc1fb2e54cdccf9bda61d6e2b784d63d05aeb3b0
Evan Beck <evandrydenbeck@gmail.com>
2023-04-07 14:00:37 -0400

    add working display

--- client/src/components/Authors.js
+++ client/src/components/Authors.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-function Authors(authors) {
+function Authors({ authors }) {
     console.log(authors);
-    // let authorsList = authors.map(author => <div>{author.name}</div>)
+    let authorsList = authors.map(author => <div>{author.name}</div>)
   return (
     <div>
         <h1>Authors</h1>
-        {/* {authorsList} */}
+        {authorsList}
     </div>
   )
 }
