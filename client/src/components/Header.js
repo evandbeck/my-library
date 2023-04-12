704cd497760ea21f10590c7f63de738b5c105872
Evan Beck <evandrydenbeck@gmail.com>
2023-04-12 10:15:52 -0400

    refactor NewBook component

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,19 @@
 import React from 'react';
+import { useState } from 'react';
+import NewBook from './NewBook';
 
 function Header() {
+    const [showAddBook, setShowAddBook] = useState(false)
+
+    function handleShowAddBook() {
+      setShowAddBook(showAddBook => !showAddBook);
+    }
   return (
-    <div>Header</div>
+    <div>
+        <h1>Header</h1>
+        <button onClick={handleShowAddBook}>{showAddBook ? "Close Form" : "Add New Book"}</button>
+        {showAddBook ? <NewBook /> : null}
+    </div>
   )
 }
 
--- client/src/components/Library.js
+++ client/src/components/Library.js
@@ -1,22 +1,22 @@
 import React from 'react';
-import { useState } from 'react';
+// import { useState } from 'react';
 import Book from './Book';
-import NewBook from './NewBook';
+// import NewBook from './NewBook';
 
 function Library({ books }) {
-    const [showAddBook, setShowAddBook] = useState(false)
+    // const [showAddBook, setShowAddBook] = useState(false)
 
     let booksList = books.map(book => <Book key={book.id} book={book} />)
 
-    function handleShowAddBook() {
-      setShowAddBook(showAddBook => !showAddBook);
-    }
+    // function handleShowAddBook() {
+    //   setShowAddBook(showAddBook => !showAddBook);
+    // }
 
   return (
     <div className="BookContainer">
         <h1>Books</h1>
-        <button onClick={handleShowAddBook}>{showAddBook ? "Close Form" : "Add New Book"}</button>
-        {showAddBook ? <NewBook /> : null}
+        {/* <button onClick={handleShowAddBook}>{showAddBook ? "Close Form" : "Add New Book"}</button>
+        {showAddBook ? <NewBook /> : null} */}
         {booksList}
     </div>
   )
