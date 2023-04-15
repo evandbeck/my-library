e752e7a5b9c423501d604b99bef869957c18689e
Evan Beck <evandrydenbeck@gmail.com>
2023-04-15 15:00:52 -0400

    add working DELETE request

--- client/src/components/Book.js
+++ client/src/components/Book.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import { useState } from 'react';
 import EditBook from './EditBook';
+import axios from 'axios';
 
 function Book({ book }) {
-  const {title, description, read, rating} = book;
+  const {id, title, description, read, rating} = book;
   
   const [editBook, setEditBook] = useState(false)
+  const [deleteBook, setDeleteBook] = useState(false)
+
+  const API_BOOKS = `http://localhost:3000/api/v1/books/${id}`;
     
     function handleShowEditBook() {
       setEditBook(editBook => !editBook)
+    }
+
+    function handleShowDeleteBook() {
+      setDeleteBook(deleteBook => !deleteBook)
+    }
+
+    function handleDeleteBook() {
+      axios.delete(API_BOOKS)
+      .then(resp => console.log(resp))
+      .catch(error => console.log(error))
     }
 
     const displayBookCard = (
@@ -18,7 +32,14 @@ function Book({ book }) {
         <p>Read: {read ? "Yes" : "No"}</p>
         <p>Rating: {rating}/5</p>
         <button onClick={handleShowEditBook}>Update</button>
-        <button>Delete</button>
+        <button onClick={handleShowDeleteBook}>Delete</button>
+        {deleteBook ? 
+          <div>
+            <p>Are you sure?</p>
+            <button onClick={handleDeleteBook}>Yes</button>
+            <button onClick={handleShowDeleteBook}>No</button>
+          </div> 
+        : null}
     </div>
     )
 
